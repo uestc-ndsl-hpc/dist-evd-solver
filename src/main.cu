ef44f03c1bd0cc9bf0a5a0fa3dad5260ba9eecd3
= <=>
2025-07-01 22:07:01 +0800

    添加子模块 fmt 和 argh，更新 .gitmodules 文件以包含它们的路径和 URL。

---
+++ src/main.cu
@@ -0,0 +1,35 @@
+#include <fmt/format.h>
+
+#include "argh.h"
+#include "log.h"
+#include "matrix_ops.cuh"
+
+template <typename T>
+void run_workflow(int n) {
+    auto C = matrix_ops::create_symmetric_random<T>(n);
+    if (util::Logger::is_verbose()) {
+        matrix_ops::print(C, n, "Final Symmetric Matrix C");
+    }
+}
+
+int main(int argc, char** argv) {
+    argh::parser cmdl(argv);
+
+    const bool verbose = cmdl[{"-v", "--verbose"}];
+    util::Logger::init(verbose);
+
+    util::Logger::println("Starting dist-evd-solver");
+
+    auto n = 4;
+    cmdl({"-n", "--size"}, 4) >> n;
+
+    if (cmdl[{"--double"}]) {
+        util::Logger::println("Using double precision");
+        run_workflow<double>(n);
+    } else {
+        util::Logger::println("Using single precision");
+        run_workflow<float>(n);
+    }
+
+    return 0;
+}
