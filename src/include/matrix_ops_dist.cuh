38a78bb9a34c33f301065ecb53a28be4f398a6ce
ruinique <rui1@std.uestc.edu.cn>
2025-08-06 18:02:26 +0800

    fix(matrix_ops): add missing newline at end of file in matrix_ops_dist.cuh

--- src/include/matrix_ops_dist.cuh
+++ src/include/matrix_ops_dist.cuh
@@ -8,6 +8,7 @@ namespace matrix_ops {
 namespace dist {
 template <typename T>
 void sy2sb(const common::CublasHandle& handle, size_t n, T* A, size_t lda, T* W,
-           size_t ldw, T* Y, size_t ldy, size_t nb = 1024, size_t b = 32, size_t gpu_num = 1);
+           size_t ldw, T* Y, size_t ldy, size_t nb = 1024, size_t b = 32,
+           size_t gpu_num = 1);
 }  // namespace dist
 }  // namespace matrix_ops
